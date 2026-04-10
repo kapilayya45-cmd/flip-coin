f3076810425b748a166774f9de44c55ba7f747a5
kapilayya45-cmd <kapilayya45@gmail.com>
2026-04-11 04:53:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ def home():
     return render_template('home.html')
 if __name__ == "__main__":
     app.run()
+    <p>🎁 Daily bonus applied!</p>
+    c.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY,
+    password TEXT,
+    coins INTEGER,
+    last_claim TEXT
+)
+''')
